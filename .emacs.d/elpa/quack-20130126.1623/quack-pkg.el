37e663b8ba10071ea25e1d18aef66e823f8702db
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2016-03-28 19:06:08 +0200

    Clojure setup

--- .emacs.d/elpa/quack-20130126.1623/quack-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "quack" "20130126.1623" "No description available." (quote nil))
--- .emacs.d/elpa/relative-line-numbers-20151006.1446/relative-line-numbers-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "relative-line-numbers" "20151006.1446" "Display relative line numbers on the margin" (quote ((emacs "24"))))
