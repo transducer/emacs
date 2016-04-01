fe5ab3ef19fcf514ef8fef09bd5f1ea5bf43a89c
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2016-04-01 22:03:20 +0200

    Start full screen

--- .emacs.d/customizations/ui.el
+++ .emacs.d/customizations/ui.el
@@ -31,10 +31,8 @@
 ;; increase font size for better readability
 (set-face-attribute 'default nil :height 140)
 
-;; Uncomment the lines below by removing semicolons and play with the
-;; values in order to set the width (in characters wide) and height
-;; (in lines high) Emacs will have whenever you start it
-;; (setq initial-frame-alist '((top . 0) (left . 0) (width . 177) (height . 53)))
+;; Start full screen
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ;; These settings relate to how emacs interacts with your operating system
 (setq ;; makes killing/yanking interact with the clipboard
