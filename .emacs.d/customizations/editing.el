80fdba03ef3527f3a91ffe33c307a001c222cb6d
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2016-06-04 01:53:58 +0200

    <C-tab> for switching to previous buffer

--- .emacs.d/customizations/editing.el
+++ .emacs.d/customizations/editing.el
@@ -69,6 +69,13 @@
       (ns-get-selection-internal 'CLIPBOARD)
     (quit nil)))
 
+(defun switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+(global-set-key (kbd "C-tab") 'switch-to-previous-buffer)
+
 (setq electric-indent-mode nil)
 
 ;; Automatic indentation
